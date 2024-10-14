4ee3f408c3e89ec3f252108066977ce00d0e9d8e
WeronikaMagdalena <weronika.wooojcik@gmail.com>
2024-10-14 13:23:13 +0200

    Spring Boot Initializr

---
+++ zpi/src/main/java/edu/pwr/zpi/ZpiApplication.java
@@ -0,0 +1,13 @@
+package edu.pwr.zpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZpiApplication.class, args);
+	}
+
+}
