d65a777416c09d9e3821b3ecc1844dc2c7d5b961
zuzaszk <zu.aszkielowicz@gmail.com>
2024-10-22 16:32:26 +0200

    Minor change

--- backend/src/main/java/com/backend/zpi/config/WebConfig.java
+++ backend/src/main/java/com/backend/zpi/config/WebConfig.java
@@ -1,6 +1,6 @@
 package com.backend.zpi.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${frontend_url}")
+    private String frontendUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(frontendUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
