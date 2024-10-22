55d7c4a696c8092cf36fafd8080f60053ea5beed
zuzaszk <zu.aszkielowicz@gmail.com>
2024-10-22 12:32:25 +0200

    Renamed the app

--- backend/src/main/java/com/backend/zpi/Application.java
+++ backend/src/main/java/com/backend/zpi/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZpiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZpiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
